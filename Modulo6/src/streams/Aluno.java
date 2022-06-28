eded7e74895a2914359baf084ab5759ad2a36a65
Windows <souza.murilo@live.com>
2022-06-28 14:26:04 -0300

    Data: 2022-JUN-25 Estudo java

--- Modulo6/src/streams/Aluno.java
+++ Modulo6/src/streams/Aluno.java
@@ -1,5 +1,7 @@
 package streams;
 
+import java.util.Objects;
+
 public class Aluno {
 
     final String nome;
@@ -17,5 +19,22 @@ public class Aluno {
     public Aluno(String nome, double nota){
         this.nome = nome;
         this.notaProva = nota;
+    }
+
+    public String toString(){
+        return nome + " tem nota "+ notaProva;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Aluno)) return false;
+        Aluno aluno = (Aluno) o;
+        return Double.compare(aluno.notaProva, notaProva) == 0 && Double.compare(aluno.notaTrabalho, notaTrabalho) == 0 && presenca == aluno.presenca && nome.equals(aluno.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, notaProva, notaTrabalho, presenca);
     }
 }
